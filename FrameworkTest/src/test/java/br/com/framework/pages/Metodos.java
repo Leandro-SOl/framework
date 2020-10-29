8504d0b64d7a43db00bce5c18327cff728afeb6a
Leandro <leandrooliveira.lo52@gmail.com>
2020-10-28 21:23:01 -0300

    inclusao do metodo navegador

--- FrameworkTest/src/test/java/br/com/framework/pages/Metodos.java
+++ FrameworkTest/src/test/java/br/com/framework/pages/Metodos.java
@@ -1,5 +1,12 @@
 package br.com.framework.pages;
 
+import org.openqa.selenium.WebDriver;
+
 public class Metodos {
+	WebDriver driver;
+
+	public void navegdor(String url, String navegador) {
+
+	}
 
 }
