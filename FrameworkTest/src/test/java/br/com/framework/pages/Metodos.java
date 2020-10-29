947ed1022f6917147e2c0e97ae9bcaf376734f72
Leandro <leandrooliveira.lo52@gmail.com>
2020-10-28 21:43:13 -0300

    inclusao do System.setProperty

--- FrameworkTest/src/test/java/br/com/framework/pages/Metodos.java
+++ FrameworkTest/src/test/java/br/com/framework/pages/Metodos.java
@@ -1,12 +1,26 @@
 package br.com.framework.pages;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class Metodos {
 	WebDriver driver;
 
 	public void navegdor(String url, String navegador) {
-
+		if (navegador == "CHROME" || navegador == "FIREFOX") {
+			if (navegador == "CHROME") {
+				System.setProperty("webdriver.chrome.driver", navegador);
+				driver = new ChromeDriver();
+				driver.manage().window().maximize();
+			} else if (navegador == "CHROME") {
+				System.setProperty("webdriver.gecko.driver", navegador);
+				driver = new FirefoxDriver();
+				driver.manage().window().maximize();
+			}
+		} else {
+			System.out.println("Digite CHROME ou FIREFOX");
+		}
 	}
 
 }
